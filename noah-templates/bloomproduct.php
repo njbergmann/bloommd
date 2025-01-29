5938026cc6c75eef46829f5b22278d2b0979d7b9
Noah Bergmann <njbergmann@gmail.com>
2025-01-28 16:42:18 -0800

    Changed template to include description

--- noah-templates/bloomproduct.php
+++ noah-templates/bloomproduct.php
@@ -2,9 +2,15 @@
     <?php while ($query->have_posts()) : $query->the_post(); ?>
         <div class="post-container">
             <div class="custom-post">
-                <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_post_thumbnail_caption(); ?>" />
-                <p class="title"><?php the_title(); ?></p>
-                <p class="company"><?php echo get_field('company_name'); ?></p>
+                <div class="custom-post-info">
+                    <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_post_thumbnail_caption(); ?>" />
+                    <p class="title"><?php the_title(); ?></p>
+                    <p class="company"><?php echo get_field('company_name'); ?></p>
+                </div>
+                <div class="custom-post-description">
+                    <p><?php the_content(); ?></p>
+                </div>
+            </div>
             </div>
         </div>
     <?php endwhile; ?>
